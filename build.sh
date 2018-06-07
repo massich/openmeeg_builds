95308f1c64c94c3b08955f1e662e8cbbc060a9f9
Joan Massich <sik@visor.udg.edu>
2018-06-07 11:55:02 +0200

    Make it verbose

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 SRC_DIR=/home/sik/code/openmeeg/
-BUILD_DIR=/home/sik/Workspace/openmeeg/build_mkl
+BUILD_DIR=/home/sik/Workspace/openmeeg/build_linux
 INSTALL_DIR=$BUILD_DIR/simulate_install_dir
 
 cd $BUILD_DIR
@@ -7,8 +7,8 @@ rm $BUILD_DIR/* -Rf
 cmake $SRC_DIR -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR  \
                -DCMAKE_CXX_STANDARD=11 \
                -DBLA_VENDOR=Intel10_64lp \
-	             -DCMAKE_PREFIX_PATH=/home/sik/miniconda3/envs/xx/lib/cmake/ \
-&& cmake --build . -- -j 8 \
-&& make install
+
+make VERBOSE=1
+make install
 
 cd -
