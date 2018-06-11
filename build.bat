3494ef68c28a9418580d2a6691c7ec84b460d3f2
Joan Massich <sik@visor.udg.edu>
2018-06-11 09:49:53 +0200

    Add vcomp_path

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@ set BUILD_DIR=E:\openmeeg\build_win
 set INSTALL_DIR=%BUILD_DIR%\simulate_install_dir
 set MY_CONDA_PATH="C:\conda\envs\matio-cmake\Library\lib\cmake"
 set CMAKE_CONFIG="Release"
-
+set VCOMP_PATH=" C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\x64\Microsoft.VC140.OpenMP\vcomp140.dll"
 set matio_dir="C:\conda\envs\matio-cmake\Library\"
 
 del /q %BUILD_DIR%\*
@@ -16,9 +16,13 @@ cmake -LAH -G "NMake Makefiles"  ^
       -DBLA_STATIC=ON                        ^
       -DCMAKE_PREFIX_PATH=%MY_CONDA_PATH%    ^
       -DCMAKE_VERBOSE_MAKEFILE=ON            ^
-      %SRC_DIR% 
+      -DVCOMP_PATH=%VCOMP_PATH%              ^
+      --trace                       ^
+      --trace-expand                ^
+      --trace-source=CMakeLists.txt ^
+      %SRC_DIR%                     
 
 cmake --build . --config %CMAKE_CONFIG% 
 cmake --build . --target install
 
-cd -
+cd ..
