01356f3a9ccbc51757aebdabf581647cee3b2649
Joan Massich <sik@visor.udg.edu>
2018-08-15 22:33:56 +0200

    Use debug info

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 SRC_DIR=/home/sik/code/openmeeg/
-BUILD_DIR=/home/sik/Workspace/openmeeg/build_linux
+BUILD_DIR=/home/sik/Workspace/openmeeg/build_linux_db
 INSTALL_DIR=$BUILD_DIR/simulate_install_dir
 
 cd $BUILD_DIR
@@ -7,6 +7,9 @@ rm $BUILD_DIR/* -Rf
 cmake $SRC_DIR -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR  \
                -DCMAKE_CXX_STANDARD=11 \
                -DBLA_VENDOR=Intel10_64lp \
+               -DCMAKE_BUILD_TYPE=Debug \
+               -DCMAKE_C_FLAGS_DEBUG="-g -O0" \
+               -DCMAKE_CXX_FLAGS_DEBUG="-g -O0" \
 
 make VERBOSE=1
 make install
